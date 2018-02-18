12ec1e12412fa1635a8ebcb2ffdd51c030510d65
Alexis Rengifo <alexisrengifo@gmail.com>
2018-02-18 12:33:07 -0500

    complete Board component

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 class Square extends React.Component {
     render() {
         return (
-            <button className="square"> 
+            <button className="square">
             {this.props.value} 
             </button>
             );
@@ -19,9 +19,30 @@ class Board extends React.Component {
     renderSquare(i) {
         return <Square value={i} />;
     }
+
+    render() {
+        const status = 'Next player: X';
+
+        return(
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+            );
+    }
 }
 
-// Game component
-class Game extends React.Component {
-    
-}
